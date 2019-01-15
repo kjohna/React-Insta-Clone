7f8c9c6ed70a63c15645e617c5898070a8e957fa
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-15 14:58:55 -0700

    add comment functionality moved to CommentSection component

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -5,44 +5,79 @@ import Comment from './Comment';
 
 import './CommentSection.css'
 
-function CommentSection(props) {
-  const comments = props.commentData.map((commentData, i) => {
-    return <Comment key={i} commentData={commentData} />
-  });
-  
-  return (
-    <div className="comment-section">
-      {comments}
-      <div className="timestamp">
-        {moment(props.postTimestamp, 'MMMM Do YYYY, hh:mm:ss a').fromNow()} {/*  "July 17th 2017, 12:42:40 pm" */}
+class CommentSection extends React.Component {
+  constructor(props){
+    super();
+    this.props = props;
+    // console.log("commentsection", props);
+    this.state = {
+      commentData: this.props.commentData,
+      newCommentText: ""
+    };
+  }
+
+  handleInput = (e) => {
+    // console.log("handleInput", e.target.name, e.target.value);
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      }
+    );
+  }
+
+  addComment = (e) => {
+    // handler function to add comment
+    e.preventDefault();
+    console.log("addcomment", this.state.newCommentText);
+    this.setState(
+      {
+        commentData: [
+          ...this.state.commentData,
+          {
+            username: "testusername",
+            text: this.state.newCommentText
+          }
+        ],
+        newCommentText: "" // reset input text
+      }
+    );
+  }  
+
+  render() {
+    const comments = this.state.commentData.map((commentData, i) => {
+      return <Comment key={i} commentData={commentData} />
+    });
+    console.log("commentSection render");
+    return (
+      <div className="comment-section">
+        {comments}
+        <div className="timestamp">
+          {moment(this.props.postTimestamp, 'MMMM Do YYYY, hh:mm:ss a').fromNow()} {/*  "July 17th 2017, 12:42:40 pm" */}
+        </div>
+        <div className="new-comment-container">
+          <form onSubmit={this.addComment}>
+            <input 
+              type="text" 
+              placeholder="Add a comment..."
+              name="newCommentText"
+              onChange={this.handleInput}
+              value={this.state.newCommentText}
+              autoComplete="off"
+            />
+          </form>
+        </div>
       </div>
-      <div className="new-comment-container">
-        <form onSubmit={props.addComment}>
-          <input 
-            type="text" 
-            placeholder="Add a comment..."
-            name="newCommentText"
-            onChange={props.newCommentHandleInput}
-            value={props.newCommentValue}
-            autoComplete="off"
-          />
-        </form>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 CommentSection.propTypes = {
   postTimestamp: PropTypes.string,
-  newCommentHandleInput: PropTypes.func,
-  newCommentValue: PropTypes.string,
-  addComment: PropTypes.func
+  newCommentValue: PropTypes.string
 }
 
 CommentSection.defaultProps = {
-  postTimestamp: '',
-  newCommentHandleInput: ()=>{},
-  newCommentValue: ''
+  postTimestamp: ''
 }
 
 export default CommentSection;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -4,61 +4,23 @@ import CommentSection from '../CommentSection/CommentSection';
 
 import './PostContainer.css';
 
-class PostContainer extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      commentData: props.post.comments,
-      newCommentText: ""
-    };
-    this.props = props;
-  }
-
-  handleInput = (e) => {
-    // console.log("handleInput", e.target.name, e.target.value);
-    this.setState(
-      {
-        [e.target.name]: e.target.value
-      }
-    );
-  }
-
-  addComment = (e) => {
-    // handler function to add comment
-    e.preventDefault();
-    this.setState(
-      {
-        commentData: [
-          ...this.state.commentData,
-          {
-            username: "testusername",
-            text: this.state.newCommentText
-          }
-        ],
-        newCommentText: "" // reset input text
-      }
-    );
-  }
-
-  render () {
-    return (
-      <div className="post-container">
-        <Post 
-          postUsername={this.props.post.username} 
-          postThumbnailUrl={this.props.post.thumbnailUrl}
-          postImageUrl={this.props.post.imageUrl}
-          postLikes={this.props.post.likes}
-        />
-        <CommentSection 
-          commentData={this.state.commentData}
-          postTimestamp={this.props.post.timestamp}
-          newCommentHandleInput={this.handleInput}
-          newCommentValue={this.state.newCommentText}
-          addComment={this.addComment}
-        />
-      </div>
-    );
-  }
+function PostContainer(props) {
+  // console.log("postcontainer", props);
+  return (
+    <div className="post-container">
+      <Post 
+        postUsername={props.post.username} 
+        postThumbnailUrl={props.post.thumbnailUrl}
+        postImageUrl={props.post.imageUrl}
+        postLikes={props.post.likes}
+      />
+      <CommentSection 
+        commentData={props.post.comments}
+        postTimestamp={props.post.timestamp}
+      />
+    </div>
+  );
+  
 }
 
 export default PostContainer;
