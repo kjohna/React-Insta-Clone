cc9d6b49e8f666f7a85063247ae388d13799b262
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-16 14:10:14 -0700

    PostsPage created, moved from app.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
-import dummyData from './dummy-data';
+import PostsPage from './components/PostContainer/PostsPage';
 
 import './App.css';
 
@@ -9,48 +7,21 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      postData: [],
-      searchText: ''
+      
     }
   }
 
   componentDidMount() {
-    // if we were using an api to get post data, it would happen here:
-    const message = dummyData;
-    // the fetch would call next line once the 'message' data was returned
     this.setState({
-      postData: message
-    });
-  }
-
-  handleSearchInput = (e) => {
-    // console.log("search input ", e.target.value.length);
-    // if anything in this.state.searchText, filter for username contains searchText otherwise return all posts
-    const searchResults = e.target.value.length > 0 ? 
-      dummyData.filter(post => post.username.includes(e.target.value))
-    : dummyData;
-    // console.log(searchResults);
-    this.setState({
-      postData: searchResults,
-      [e.target.name]: e.target.value
+      
     });
   }
 
   render() {
-    // console.log(this.state.postData);
-    const posts = this.state.postData.length > 0 ? 
-      this.state.postData.map(post => {
-        return <PostContainer key={post.timestamp} post={post} />
-      }) :
-      null;
 
     return (
       <div className="App">
-        <SearchBar 
-          handleSearchInput={this.handleSearchInput}
-          searchText={this.state.searchText}
-        />
-        {posts}
+        <PostsPage />
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PostContainer from './PostContainer';
+import SearchBar from '../SearchBar/SearchBar';
+import dummyData from '../../dummy-data';
+
+import './PostContainer.css';
+
+class PostsPage extends Component {
+  constructor(){
+    super();
+    this.state = {
+      postData: [],
+      searchText: ''
+    }
+  }
+
+  componentDidMount() {
+    // console.log("posts page CDM");
+    // if we were using an api to get post data, it would happen here:
+    const message = dummyData;
+    // the fetch would call next line once the 'message' data was returned
+    this.setState({
+      postData: message
+    });
+  }
+
+  handleSearchInput = (e) => {
+    // console.log("search input ", e.target.value.length);
+    // if anything in this.state.searchText, filter for username contains searchText otherwise return all posts
+    const searchResults = e.target.value.length > 0 ? 
+      dummyData.filter(post => post.username.includes(e.target.value))
+    : dummyData;
+    // console.log(searchResults);
+    this.setState({
+      postData: searchResults,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
+    // console.log("PostsPage: ", this.state.postData.length);
+    const posts = this.state.postData.length > 0 ? 
+      this.state.postData.map(post => {
+        return <PostContainer key={post.timestamp} post={post} />
+      }) :
+      null;
+
+    return (
+      <div className="posts-page">
+        <SearchBar 
+          handleSearchInput={this.handleSearchInput}
+          searchText={this.state.searchText}
+        />
+        {posts}
+      </div>
+    );
+  }
+}
+
+export default PostsPage;
