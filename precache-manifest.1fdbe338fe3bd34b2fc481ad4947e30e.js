fc8f5406114a7cc5de3dee87f6adaa3d5059658d
kjohna <43828763+kjohna@users.noreply.github.com>
2019-05-21 13:54:33 -0600

    Updates

---
+++ precache-manifest.1fdbe338fe3bd34b2fc481ad4947e30e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f230aae4d76823529209",
+    "url": "/React-Insta-Clone/instagram/static/css/main.422624be.chunk.css"
+  },
+  {
+    "revision": "f230aae4d76823529209",
+    "url": "/React-Insta-Clone/instagram/static/js/main.f230aae4.chunk.js"
+  },
+  {
+    "revision": "b931b8cb63a6cec642a1",
+    "url": "/React-Insta-Clone/instagram/static/js/1.b931b8cb.chunk.js"
+  },
+  {
+    "revision": "89208d060329b453a2dd",
+    "url": "/React-Insta-Clone/instagram/static/js/runtime~main.89208d06.js"
+  },
+  {
+    "revision": "ff854302b1aeaf008c572d7d015a0317",
+    "url": "/React-Insta-Clone/instagram/static/media/iglogo.ff854302.png"
+  },
+  {
+    "revision": "de94a072786ab64b677d46abc52d85ba",
+    "url": "/React-Insta-Clone/instagram/static/media/heart.de94a072.svg"
+  },
+  {
+    "revision": "a388b7e163da1c418d69b3889931be44",
+    "url": "/React-Insta-Clone/instagram/static/media/camera.a388b7e1.svg"
+  },
+  {
+    "revision": "ababa6bdeac00b17f22fc7e7abc9f236",
+    "url": "/React-Insta-Clone/instagram/static/media/compass.ababa6bd.svg"
+  },
+  {
+    "revision": "4003754b4c8d826ba3b601e6641cc6ea",
+    "url": "/React-Insta-Clone/instagram/index.html"
+  }
+];
